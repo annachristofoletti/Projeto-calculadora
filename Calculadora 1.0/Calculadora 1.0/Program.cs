3f38cf2b6bcbd06bdb9e92a30f7c43f0fbcdbf56
annachristofoletti <annachristofoletti1@gmail.com>
2025-02-27 19:56:58 -0300

    redigitação do código

---
+++ Calculadora 1.0/Calculadora 1.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double numero1, numero2;
+
+
+            Console.WriteLine("Digite o primeiro numero: ");
+            numero1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            numero2 = double.Parse(Console.ReadLine());
+
+            double subtracao = numero1 - numero2;
+            double soma = numero1 + numero2;
+            double multiplicacao = numero1 * numero2;
+            double divisao = numero1 / numero2;
+
+            Console.WriteLine("A subtração dos numeros é:" + subtracao);
+            Console.WriteLine("A soma dos números é: " + soma);
+            Console.WriteLine("A multiplicação dos números é: " + multiplicacao);
+            Console.WriteLine("A divisão dos numeros é:" + divisao);
+        }
+    }
+}
